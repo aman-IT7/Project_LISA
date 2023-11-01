3df1954a3cdcbb3cc832a943583067f648fafdf1
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-11-01 12:19:40 +0530

    moisture prediction

--- water irrigation/data_cleaning.py
+++
@@ -1,9 +0,0 @@
-# Latitude: 34 deg; 54 min N
-# Longitude: 86 deg; 36 min W
-
-
-# Latitude: 34.9 degrees North
-# Longitude: -86.6 degrees (West longitudes are represented as negative values)
-
-
-lat, long = (34.6, -86.6)  # location of bragg farm
---
+++ water irrigation/info.py
@@ -0,0 +1,12 @@
+# Latitude: 34 deg; 54 min N
+# Longitude: 86 deg; 36 min W
+
+
+# Latitude: 34.9 degrees North
+# Longitude: -86.6 degrees (West longitudes are represented as negative values)
+
+
+lat, long = (34.6, -86.6)  # location of bragg farm
+
+
+dataset_columns_preedit = """ Site Id	Date	Time	SMS.I-1:-2 (pct)  (loam)	SMS.I-1:-4 (pct)  (loam)	SMS.I-1:-8 (pct)  (loam)	SMS.I-1:-20 (pct)  (loam)	SMS.I-1:-40 (pct)  (loam)	STO.I-1:-2 (degC) 	STO.I-1:-4 (degC) 	STO.I-1:-8 (degC) 	STO.I-1:-20 (degC) 	STO.I-1:-40 (degC) 	tempmax	tempmin	temp	feelslikemax	feelslikemin	feelslike	dew	humidity	precip	precipprob	precipcover	preciptype	snow	snowdepth	windgust	windspeed	winddir	sealevelpressure	cloudcover	visibility	solarradiation	solarenergy	uvindex	severerisk	sunrise	sunset	moonphase	conditions	description	icon	stations """

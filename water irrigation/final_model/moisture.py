e630372c9b389178a19fceedb23296280e7ad46f
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-12-02 19:26:17 +0530

    more data

--- water irrigation/final_model/moisture.py
+++ water irrigation/final_model/moisture.py
@@ -4,11 +4,12 @@ from dependancies import *
 from io import StringIO
 
 json_input = StringIO(input())
+
+# sample data to test
+# test = """ {"ST_4": 30.4, "temp": 40.0, "humidity": 70.27, "precip": 0.0, "windspeed": 10.7, "cloudcover": 50.2, "conditions": "Clear"} """
+
 data = pd.read_json(json_input, orient="index").T
 
 
 predicted_moisture = MoiturePrediction().Predict(data)
 print(predicted_moisture)
-
-
-test = """ {"ST_4": 27.4, "temp": 25.0, "humidity": 80.27, "precip": 0.0, "windspeed": 2.7, "cloudcover": 14.2, "conditions": "Clear"} """
